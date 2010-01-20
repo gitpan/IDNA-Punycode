eda7e5e7fbb785271e5d3215ddc323d640c1da28
Claus Färber <CFAERBER@cpan.org>
2010-01-20 11:42:06 +0000

    Import of CFAERBER/IDNA-Punycode-1.000 from CPAN.
    gitpan-cpan-distribution: IDNA-Punycode
    gitpan-cpan-version:      1.000
    gitpan-cpan-path:         CFAERBER/IDNA-Punycode-1.000.tar.gz
    gitpan-cpan-author:       CFAERBER
    gitpan-cpan-maturity:     released

--- lib/IDNA/Punycode.pm
+++ lib/IDNA/Punycode.pm
@@ -1,165 +1,41 @@
 package IDNA::Punycode;
 
 use strict;
-our $VERSION = 0.03;
+use utf8;
+use warnings;
+
+our $VERSION = "1.000";
+$VERSION = eval $VERSION;
 
 require Exporter;
 our @ISA	= qw(Exporter);
-our @EXPORT = qw(encode_punycode decode_punycode idn_prefix);
+our @EXPORT 	= qw(encode_punycode decode_punycode idn_prefix);
 
-use integer;
+use Net::IDN::Punycode();
+use Net::IDN::Encode();
 
-our $DEBUG = 0;
 our $PREFIX = 'xn--';
-
-use constant BASE => 36;
-use constant TMIN => 1;
-use constant TMAX => 26;
-use constant SKEW => 38;
-use constant DAMP => 700;
-use constant INITIAL_BIAS => 72;
-use constant INITIAL_N => 128;
-
-my $Delimiter = chr 0x2D;
-my $BasicRE   = qr/[\x00-\x7f]/;
-
-sub _croak { require Carp; Carp::croak(@_); }
 
 sub idn_prefix {
 	$PREFIX = shift;
 }
 
-sub digit_value {
-	my $code = shift;
-	return ord($code) - ord("A") if $code =~ /[A-Z]/;
-	return ord($code) - ord("a") if $code =~ /[a-z]/;
-	return ord($code) - ord("0") + 26 if $code =~ /[0-9]/;
-	return;
-}
-
-sub code_point {
-	my $digit = shift;
-	return $digit + ord('a') if 0 <= $digit && $digit <= 25;
-	return $digit + ord('0') - 26 if 26 <= $digit && $digit <= 36;
-	die 'NOT COME HERE';
-}
-
-sub adapt {
-	my($delta, $numpoints, $firsttime) = @_;
-	$delta = $firsttime ? $delta / DAMP : $delta / 2;
-	$delta += $delta / $numpoints;
-	my $k = 0;
-	while ($delta > ((BASE - TMIN) * TMAX) / 2) {
-		$delta /= BASE - TMIN;
-		$k += BASE;
-	}
-	return $k + (((BASE - TMIN + 1) * $delta) / ($delta + SKEW));
-}
-
 sub decode_punycode {
-	my $code = shift;
-
-	my $n	  = INITIAL_N;
-	my $i	  = 0;
-	my $bias   = INITIAL_BIAS;
-	my @output;
-
 	if ($PREFIX) {
-		if ($code !~ /^$PREFIX/) {
-			return $code;
-		}
-		$code =~ s/^$PREFIX//;
+		local $Net::IDN::Encode::IDNA_prefix = $PREFIX;
+		return Net::IDN::Encode::_to_unicode(shift);
+	} else {
+		return Net::IDN::Punycode::decode_punycode(shift);
 	}
-
-	if ($code =~ s/(.*)$Delimiter//o) {
-		push @output, map ord, split //, $1;
-		return _croak('non-basic code point') unless $1 =~ /^$BasicRE*$/o;
-	}
-
-	while ($code) {
-		my $oldi = $i;
-		my $w	= 1;
-		LOOP:
-		for (my $k = BASE; 1; $k += BASE) {
-			my $cp = substr($code, 0, 1, '');
-			my $digit = digit_value($cp);
-			defined $digit or return _croak("invalid punycode input");
-			$i += $digit * $w;
-			my $t = ($k <= $bias) ? TMIN
-			: ($k >= $bias + TMAX) ? TMAX : $k - $bias;
-			last LOOP if $digit < $t;
-			$w *= (BASE - $t);
-		}
-		$bias = adapt($i - $oldi, @output + 1, $oldi == 0);
-		warn "bias becomes $bias" if $DEBUG;
-		$n += $i / (@output + 1);
-		$i = $i % (@output + 1);
-		splice(@output, $i, 0, $n);
-		warn join " ", map sprintf('%04x', $_), @output if $DEBUG;
-		$i++;
-	}
-	return join '', map chr, @output;
 }
 
 sub encode_punycode {
-	my $input = shift;
-	# my @input = split //, $input; # doesn't work in 5.6.x!
-	my @input = map substr($input, $_, 1), 0..length($input)-1;
-
-	my $n	 = INITIAL_N;
-	my $delta = 0;
-	my $bias  = INITIAL_BIAS;
-
-	my @output;
-	my @basic = grep /$BasicRE/, @input;
-	my $h = my $b = @basic;
-	#push @output, @basic, $Delimiter if $b > 0;
-	push @output, @basic if $b > 0;
-	warn "basic codepoints: (@output)" if $DEBUG;
-
-	if ($h < @input) {
-		$PREFIX && unshift(@output, $PREFIX);
-		push(@output, $Delimiter);
+	if ($PREFIX) {
+		local $Net::IDN::Encode::IDNA_prefix = $PREFIX;
+		return Net::IDN::Encode::_to_ascii(shift);
 	} else {
-		return join '', @output;
+		return Net::IDN::Punycode::encode_punycode(shift);
 	}
-
-	while ($h < @input) {
-		my $m = min(grep { $_ >= $n } map ord, @input);
-		warn sprintf "next code point to insert is %04x", $m if $DEBUG;
-		$delta += ($m - $n) * ($h + 1);
-		$n = $m;
-		for my $i (@input) {
-			my $c = ord($i);
-			$delta++ if $c < $n;
-			if ($c == $n) {
-				my $q = $delta;
-				LOOP:
-				for (my $k = BASE; 1; $k += BASE) {
-					my $t = ($k <= $bias) ? TMIN :
-					($k >= $bias + TMAX) ? TMAX : $k - $bias;
-					last LOOP if $q < $t;
-					my $cp = code_point($t + (($q - $t) % (BASE - $t)));
-					push @output, chr($cp);
-					$q = ($q - $t) / (BASE - $t);
-				}
-				push @output, chr(code_point($q));
-				$bias = adapt($delta, $h + 1, $h == $b);
-				warn "bias becomes $bias" if $DEBUG;
-				$delta = 0;
-				$h++;
-			}
-		}
-		$delta++;
-		$n++;
-	}
-	return join '', @output;
-}
-
-sub min {
-	my $min = shift;
-	for (@_) { $min = $_ if $_ <= $min }
-	return $min;
 }
 
 1;
@@ -167,74 +43,89 @@ __END__
 
 =head1 NAME
 
-IDNA::Punycode - encodes Unicode string in Punycode
-
-=head1 SYNOPSIS
-
-  use IDNA::Punycode;
-  idn_prefix('xn--');
-  $punycode = encode_punycode($unicode);
-  $unicode  = decode_punycode($punycode);
+IDNA::Punycode - DEPRECATED module for IDNA and Punyode
 
 =head1 DESCRIPTION
 
-IDNA::Punycode is a module to encode / decode Unicode strings into
-Punycode, an efficient encoding of Unicode for use with IDNA.
+This module is deprecated.
 
-This module requires Perl 5.6.0 or over to handle UTF8 flagged Unicode
-strings.
+Please use L<Net::IDN::Encode> to handle full domain names and
+L<Net::IDN::Punycode> for raw I<Punycode> encoding.
+
+This module is provided for compatibility with earlier versions of
+C<IDNA::Punycode>.
 
 =head1 FUNCTIONS
 
-This module exports following functions by default.
+The following functions are imported by default. If you also C<use Net::IDN::Punycode>,
+be sure to disable import from this module:
 
-=over 4
+  use IDNA::Punycode();
 
-=item encode_punycode
+=over
 
-  $punycode = encode_punycode($unicode);
+=item idn_prefix($prefix)
 
-takes Unicode string (UTF8-flagged variable) and returns Punycode
-encoding for it.
+Sets C<$IDNA::Punycode::PREFIX> to C<$prefix>.
 
-=item decode_punycode
+B<Do not use this function> in larger applications or environments
+in which multiple application share global variables (such as
+L<mod_perl>). Instead, set the variable locally:
 
-  $unicode = decode_punycode($punycode)
+  local $IDNA::Punycode::PREFIX = 'yo--';
 
-takes Punycode encoding and returns original Unicode string.
+=item encode_punycode($input)
 
-=item idn_prefix
+If C<$IDNA::Punycode::PREFIX> is C<''>, encodes C<$input> with
+Punycode.
 
-  idn_prefix($prefix);
+If C<$IDNA::Punycode::PREFIX> is not C<''>, encodes C<$input> with
+Punycode and adds the prefix if C<$input> does contain non-base
+characters (S<i. e.> characters above U+007F). If C<$input> does
+not contain any non-base characters, it is returned as-is.
 
-causes encode_punycode() to add $prefix to ACE-string after conversion.
-As a side-effect decode_punycode() will only consider strings
-beginning with $prefix as punycode representations.
+This function does not do any string preparation as specified by
+I<nameprep> or other I<stringprep> profiles.  Use
+L<Net::IDN::Encode> if you just want to convert a domain name.
 
-According to RFC 3490 the ACE prefix "xn--" had been chosen as the
-standard.  Thus, "xn--" is also the default ACE prefix.  For compatibility
-I'm leaving idn_prefix() in the module.  Use C<idn_prefix(undef)> to
-get the old behaviour.
+This function will croak on invalid input.
+
+=item decode_punycode($input)
+
+If C<$IDNA::Punycode::PREFIX> is C<''>, decodes C<$input> with
+Punycode.
+
+If C<$IDNA::Punycode::PREFIX> is not C<''>, checks whether
+C<$input> starts with the prefix. If C<$input> starts with the
+prefix, the prefix is removed and the remainder is decoded with
+Punycode. If C<$input> does not start with the prefix, it is
+returned as-is.
+
+Of course, this function does not do any string preparation as
+specified by I<nameprep> or other I<stringprep> profiles (or some
+sort of de-preparation).
+
+This function will croak on invalid input.
 
 =back
 
-These functions throws exceptionsn on failure. You can catch 'em via
-C<eval>.
-
 =head1 AUTHORS
 
-Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt> is the original
-author and wrote almost all the code.
+Claus FE<auml>rber <CFAERBER@cpan.org>
 
-Robert Urban E<lt>urban@UNIX-Beratung.deE<gt> added C<idn_prefix()>.
+Previous version written by Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+and extended by Robert Urban E<lt>urban@UNIX-Beratung.deE<gt>.
+
+=head1 LICENSE
+
+Copyright 2007-2010 Claus FE<auml>rber.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-http://www.ietf.org/internet-drafts/draft-ietf-idn-punycode-01.txt
-
-L<Encode::Punycode>
+L<Encode>, L<Net::IDN::Encode>, L<Net::IDN::Punycode>,
+S<RFC 3492> (L<http://www.ietf.org/rfc/rfc3492.txt>)
 
 =cut
